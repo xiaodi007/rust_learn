090fc60d13fa7aab9483aa637e52c37e09f108c1
xiaodi <334830452@qq.com>
2022-07-23 16:23:47 +0800

    class5 new

--- class5/lib.rs
+++ class5/lib.rs
@@ -1,7 +1,7 @@
 //! Substrate Proof-of-Existence Pallet
 #![cfg_attr(not(feature = "std"), no_std)]
 
-// Re-export pallet items so that they can be accessed from the crate namespace.
+
 pub use pallet::*;
 
 #[frame_support::pallet]
@@ -9,24 +9,20 @@ pub mod pallet {
 	use frame_support::{pallet_prelude::*, storage::bounded_vec::BoundedVec};
 	use frame_system::pallet_prelude::*;
 
-	// Define the pallet struct placeholder, various pallet function are implemented on it.
+
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
+
 	#[pallet::config]   // <-- Step 2. code block will replace this.
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		/// For constraining the maximum bytes of a hash used for any proof
 		type MaxBytesInHash: Get<u32>;
 	}
 
-	// Pallets use events to inform users when important changes are made.
-	// Event documentation should end with an array that provides descriptive names for parameters.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
-	#[pallet::event]   // <-- Step 3. code block will replace this.
+	#[pallet::event]   
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Event emitted when a proof has been claimed. [who, claim]
@@ -36,18 +32,15 @@ pub mod pallet {
         ClaimTrans(T::AccountId, BoundedVec<u8, T::MaxBytesInHash>, T::AccountId)
 	}
 
-	#[pallet::error]   // <-- Step 4. code block will replace this.
+	#[pallet::error]   
 	pub enum Error<T> {
-		/// The proof has already been claimed.
+
 		ProofAlreadyClaimed,
-		/// The proof does not exist, so it cannot be revoked.
 		NoSuchProof,
-		/// The proof is claimed by another account, so caller can't revoke it.
 		NotProofOwner,
 	}
 
-	#[pallet::storage]   // <-- Step 5. code block will replace this.
-	/// Maps each proof to its owner and block number when the proof was made
+	#[pallet::storage] 
 	pub(super) type Proofs<T: Config> = StorageMap<
 		_,
 		Blake2_128Concat,
@@ -56,31 +49,22 @@ pub mod pallet {
 		OptionQuery,
 	>;
 
-	// Dispatchable functions allow users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-	#[pallet::call]   // <-- Step 6. code block will replace this.
+
+	#[pallet::call]  
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(1_000)]
 		pub fn create_claim(
 			origin: OriginFor<T>,
 			proof: BoundedVec<u8, T::MaxBytesInHash>,
 		) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
 			let sender = ensure_signed(origin)?;
 
-			// Verify that the specified proof has not already been claimed.
 			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
 
-			// Get the block number from the FRAME System pallet.
 			let current_block = <frame_system::Pallet<T>>::block_number();
 
-			// Store the proof with the sender and block number.
 			Proofs::<T>::insert(&proof, (&sender, current_block));
 
-			// Emit an event that the claim was created.
 			Self::deposit_event(Event::ClaimCreated(sender, proof));
 
 			Ok(())
@@ -92,25 +76,23 @@ pub mod pallet {
 			proof: BoundedVec<u8, T::MaxBytesInHash>,
             toin: T::AccountId,
 		) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
+
 			let sender = ensure_signed(origin)?;
 
             ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
             let (owner, _) = Proofs::<T>::get(&proof).expect("All proofs must have an owner!");
 
-			// Verify that sender of the current call is the claim owner.
+
 			ensure!(sender == owner, Error::<T>::NotProofOwner);
 
-			// Get the block number from the FRAME System pallet.
+
 			let current_block = <frame_system::Pallet<T>>::block_number();
             Proofs::<T>::remove(&proof);
 
-			// Store the proof with the sender and block number.
+
 			Proofs::<T>::insert(&proof, (&toin, current_block));
 
-			// Emit an event that the claim was created.
+
 			Self::deposit_event(Event::ClaimTrans(sender, proof, toin));
 
 			Ok(())
@@ -121,25 +103,22 @@ pub mod pallet {
 			origin: OriginFor<T>,
 			proof: BoundedVec<u8, T::MaxBytesInHash>,
 		) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
+
 			let sender = ensure_signed(origin)?;
 
-			// Verify that the specified proof has been claimed.
+
 			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
 
-			// Get owner of the claim.
-			// Panic condition: there is no way to set a `None` owner, so this must always unwrap.
+
 			let (owner, _) = Proofs::<T>::get(&proof).expect("All proofs must have an owner!");
 
-			// Verify that sender of the current call is the claim owner.
+
 			ensure!(sender == owner, Error::<T>::NotProofOwner);
 
-			// Remove claim from storage.
+
 			Proofs::<T>::remove(&proof);
 
-			// Emit an event that the claim was erased.
+
 			Self::deposit_event(Event::ClaimRevoked(sender, proof));
 			Ok(())
 		}
